34174f0046ec674b5ae2008b75c888453e94873a
Travis Pettrey <travis.pettrey@gmail.com>
2020-03-10 09:57:06 -0400

    initial commit

---
+++ src/main/java/com/WCCI/module5demo/Module5DemoApplication.java
@@ -0,0 +1,13 @@
+package com.WCCI.module5demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module5DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module5DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WCCI/module5demo/Module5DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WCCI.module5demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module5DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
